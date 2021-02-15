3ec96667bda1e0fc638235f1a109469c61675edf
happycodingpy <hugo.orlando2030@gmail.com>
2021-02-15 13:23:05 -0300

    Coding Challenge 1

--- coding-challenge/03-Developer-Skills/coding-challenge-1/script.js
+++ coding-challenge/03-Developer-Skills/coding-challenge-1/script.js
@@ -16,13 +16,23 @@
 // § Data 1: [17, 21, 23]
 // § Data 2: [12, 5, -5, 0, 4]
 
+//  1) Uderstanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X day? Answer: index + 1
+
+// 2) Breaking up into sub-problems
+//- Transform arary to intro string
+//- Transform each element to string with ºC
+// - Strings need to contain day (index + 1)
+// - Add ... between elements and start and end of string
+
 const data1 = [17, 21, 23];
 const data2 = [12, 5, -5, 0, 4];
 
 const printForecast = arr => {
   let day = '';
   for (let i = 0; i < arr.length; i++) {
-    day += `${arr[i]}ºC in ${i + 1} days, `;
+    day += `${arr[i]}ºC in ${i + 1} days... `;
   }
   console.log(day);
   return day;
