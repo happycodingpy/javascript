da71dbd7dd216de54fa62c03ca3834f4b54277da
happycodingpy <hugo.orlando2030@gmail.com>
2021-03-11 13:58:00 -0300

    data structures

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -96,6 +96,29 @@ const restaurant = {
   // },
 };
 
+// Property Names
+const properties = Object.keys(openingHours);
+console.log(properties);
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day1 of properties) {
+  openStr += `${day1},`;
+}
+console.log(openStr);
+
+// Porperty Values
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entrie object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+// [key, value]
+for (const [day1, { open, close }] of entries) {
+  console.log(`On ${day1} we open at ${open} and close at ${close}`);
+}
+
 // Optional Chaining
 console.log('Optional Chaining');
 if (restaurant.openingHours && restaurant.openingHours.mon)
@@ -108,20 +131,20 @@ if (restaurant.openingHours && restaurant.openingHours.mon)
 const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 for (const day of days) {
   console.log(day);
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`On ${day} we open at ${open}`);
+  //const open = restaurant.openingHours[day]?.open ?? 'closed';
+  //console.log(`On ${day} we open at ${open}`);
 }
 
 // Methods
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 
 // Arrays
 const users = [{ myName: 'Jonas', country: 'USA' }];
-console.log(users[0]?.myName ?? 'User array empty');
+// console.log(users[0]?.myName ?? 'User array empty');
 
-if (users.length > 0) console.log(users[0]?.myName);
-else console.log('User array empty');
+// if (users.length > 0) console.log(users[0]?.myName);
+// else console.log('User array empty');
 
 // Looping Array - For-or loop
 console.log('---For-or Loop---');
