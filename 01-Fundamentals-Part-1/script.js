4fc88397efcf117442c7c47c099a8773044eb3e1
happycodingpy <hugo.orlando2030@gmail.com>
2021-06-17 17:58:35 -0300

    js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -4,3 +4,7 @@ console.log(`${userName} is reading the ebook ${ebook}`);
 
 let js = 'Amazing!';
 console.log(`JavaScript is ${js}`);
+
+let firstName = 'Matilda';
+let firstNamePerson = 'John';
+console.log(`${firstName} and ${firstNamePerson} are programming languages`);
