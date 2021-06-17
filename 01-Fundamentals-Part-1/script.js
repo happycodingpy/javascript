d3ffc2d4834d25e5b412e83c7def12fb9c22b3ca
happycodingpy <hugo.orlando2030@gmail.com>
2021-06-17 18:01:42 -0300

    js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,6 @@
+////////////////////////////////////
+// Values and Variables
+
 let userName = 'Jonas';
 let ebook = 'Around the world';
 console.log(`${userName} is reading the ebook ${ebook}`);
@@ -8,3 +11,19 @@ console.log(`JavaScript is ${js}`);
 let firstName = 'Matilda';
 let firstNamePerson = 'John';
 console.log(`${firstName} and ${firstNamePerson} are programming languages`);
+
+// Variable name conventions
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
