148bff28e2cfb27834d72f3089c6e89c9895bc35
happycodingpy <hugo.orlando2030@gmail.com>
2021-06-21 16:00:21 -0300

    modal

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,9 +5,6 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
 const openModal = function () {
   modal.classList.toggle('hidden');
   overlay.classList.toggle('hidden');
@@ -18,11 +15,15 @@ const closeModal = function () {
   overlay.classList.toggle('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
+for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
-}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
