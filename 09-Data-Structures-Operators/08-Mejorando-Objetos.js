8c49c8599ccd667bd0acbe0d326e05b9a24decfa
happycodingpy <hugo.orlando2030@gmail.com>
2021-06-29 16:32:59 -0300

    Mejorando objetos

---
+++ 09-Data-Structures-Operators/08-Mejorando-Objetos.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const openingHours = {
+  thu: { open: 12, close: 22 },
+  fri: { open: 11, close: 23 },
+  sat: { open: 0, close: 24 },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // Desde ES6 declaramos los objetos de esta forma dentro
+  // de otro objeto y esto mejora el rendimiento
+  openingHours,
+
+  // Funcion que recibe dos parametros (starterIndex, mainIndex)
+  order: function (starterIndex, mainIndex) {
+    // Esta funcion retorna de starterMenu los parametros (starterIndex, mainIndex)
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ time, adress, mainIndex, starterIndex }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
