744fb1c326ca9ff89c3c967cb511a115fe00ab19
happycodingpy <hugo.orlando2030@gmail.com>
2021-07-02 13:07:43 -0300

    Sets

---
+++ 09-Data-Structures-Operators/11-Sets.js
@@ -0,0 +1,26 @@
+// Es una colección de valores únicos, lo que significa que los `Sets` nunca pueden tener duplicados y esa propiedad lo hace útiles para ciertas situaciones
+const num = [1, 2, 3, 3, 3, 3, 4, 5];
+
+const str = [
+  'Paraguay',
+  'Paraguay',
+  'Francia',
+  'Argentina',
+  'Sudafrica',
+  'Sudafrica',
+];
+
+const orderSet = new Set(num);
+console.log(orderSet);
+
+const orderSet1 = new Set(str);
+console.log(orderSet1);
+
+console.log(orderSet.has('Pizza'));
+console.log(orderSet.has('Argentina'));
+
+orderSet1.add('Australia');
+console.log(orderSet1);
+
+orderSet1.delete('Francia');
+console.log(orderSet1);
