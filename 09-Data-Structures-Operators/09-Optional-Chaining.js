9e246a592d31100efd3fe6a75cdadf8120142c91
happycodingpy <hugo.orlando2030@gmail.com>
2021-07-02 13:07:10 -0300

    Optional Chaining

--- 09-Data-Structures-Operators/09-Optional-Chaining.js
+++ 09-Data-Structures-Operators/09-Optional-Chaining.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
 const openingHours = {
-  thu: { open: 12, close: 22 },
-  fri: { open: 11, close: 23 },
-  sat: { open: 0, close: 24 },
+  [weekdays[3]]: { open: 12, close: 22 },
+  [weekdays[4]]: { open: 11, close: 23 },
+  [weekdays[5]]: { open: 0, close: 24 },
 };
 
 const restaurant = {
@@ -41,7 +43,34 @@ const restaurant = {
   },
 };
 
-// Optional chaining "?"
-// El ? permite que no se visualice el error como tal
-// sino que imprima el undefined
+// El Optional Chaining "?" previene cualquier tipo de error que no podamos contemplar a simple vista
 console.log(restaurant.openingHours.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  //   const open = restaurant.openingHours[day]?.open || closed;
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+console.log(restaurant.cantar?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [
+  { name: 'Jonas', email: 'jonas@hola.com' },
+  { name: 'Michael', email: 'michael@hola.com' },
+];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+// console.log(users[0]?.name ?? 'User array empty');
+// Es lo mismo que hacer esto
+if (users.length > 0) {
+  console.log(users[0].name);
+} else {
+  console.log('User array empty');
+}
