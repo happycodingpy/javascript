c6f50c4311ff57e42fa745a60fddfeb6d4b30fdf
happycodingpy <hugo.orlando2030@gmail.com>
2021-07-05 10:12:23 -0300

    Maps

--- 09-Data-Structures-Operators/12-Maps.js
+++ 09-Data-Structures-Operators/12-Maps.js
@@ -1,48 +1,5 @@
 'use strict';
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-
-const openingHours = {
-  [weekdays[3]]: { open: 12, close: 22 },
-  [weekdays[4]]: { open: 11, close: 23 },
-  [weekdays[5]]: { open: 0, close: 24 },
-};
-
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  // Desde ES6 declaramos los objetos de esta forma dentro
-  // de otro objeto y esto mejora el rendimiento
-  openingHours,
-
-  // Funcion que recibe dos parametros (starterIndex, mainIndex)
-  order: function (starterIndex, mainIndex) {
-    // Esta funcion retorna de starterMenu los parametros (starterIndex, mainIndex)
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery: function ({ time, adress, mainIndex, starterIndex }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`
-    );
-  },
-
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(
-      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
-  },
-
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
-};
-
 const rest = new Map();
 // set recibe los valores
 rest.set('name', 'Clasico Argentino');
@@ -62,3 +19,47 @@ rest
 
 const time = 12;
 console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+
+console.log(rest.size);
+
+const arr = [1, 2, 3];
+
+const arrMap = new Map();
+arrMap.set(arr, 'Test');
+console.log(arrMap.get(arr));
+
+///////////////////////////////////////
+// Maps: Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'Go'],
+  [2, 'Python'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+const answer = 3;
+
+console.log(question.get(question.get('correct') === answer));
+
+const paises = new Map([
+  [1, 'Argentina'],
+  [2, 'Paraguay'],
+  [3, 'Brasil'],
+  [4, 'Uruguay'],
+]);
+console.log(paises);
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Paises ${key}: ${value}`);
+}
