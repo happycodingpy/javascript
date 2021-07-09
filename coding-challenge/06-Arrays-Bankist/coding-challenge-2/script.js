8cd0da1fa358df8d57c454c58de5f7b8c87d8f11
happycodingpy <hugo.orlando2030@gmail.com>
2021-07-09 16:19:05 -0300

    challenge

---
+++ coding-challenge/06-Arrays-Bankist/coding-challenge-2/script.js
@@ -0,0 +1,12 @@
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+
+  const adults = humanAges.filter(age => age >= 18);
+
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+  return average;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
